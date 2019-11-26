e6c48721e788922b023abe6ac8eea4aadd7a8bba
Jonathan Ferreira <48031702+Sarodace@users.noreply.github.com>
2019-11-25 21:08:23 -0500

    Create ID2.c
    2nd solution, only took me 9 months /s
    Honestly, I need to make a habit of consistently solving these :P

---
+++ Solutions/ID2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+  // Define variables
+  int num1 = 1;
+  int num2 = 2;
+  int sum = 0;
+
+  while (num1 < 4000000) {
+    // Print 1st number and 2nd number
+    printf("%d, %d\n",num1,num2);
+
+    // Increment sum
+    if (!(num1 % 2))
+      sum += num1;
+    if (!(num2 % 2))
+      sum += num2;
+    
+    // Update values
+    num1 += num2;
+    num2 += num1;
+
+    // Print current sum
+    printf("Sum: %d \n",sum);
+  }
+}

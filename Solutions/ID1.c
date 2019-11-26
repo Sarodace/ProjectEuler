b667569b31e1e4e0ac52f8cdec9e0b36df1f7df5
Jonathan Ferreira <48031702+Sarodace@users.noreply.github.com>
2019-11-25 21:22:42 -0500

    Improved old code

--- Solutions/ID1.c
+++ Solutions/ID1.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
+  // Declare + define variables
   int i;
-  int sum;
-  sum = 0;
-  for (i = 1;i<1000;i++){
-    if (i % 5 == 0){
-      sum = sum + i;
-    } else if (i % 3 == 0){
-      sum = sum + i;
+  int sum = 0;
+
+  for (i = 1; i < 1000; i++){
+    // Increment sum
+    if (!(i % 5)) {
+      sum += i;
+    } else if (!(i % 3)) {
+      sum += i;
     }
   }
+  // Print solution
   printf("%d",sum);
-  return 0;
 }
